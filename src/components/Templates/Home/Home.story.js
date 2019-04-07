f901ad19a46d8aa65c8a4d1b4f7d765dd5a3426c
Joby Bejoy <jobyvarghesebejoy@karunya.edu.in>
2019-04-07 16:36:32 +0400

    Template | Home < Minor Updates >

--- src/components/Templates/Home/Home.story.js
+++ src/components/Templates/Home/Home.story.js
@@ -12,7 +12,39 @@ const stories = storiesOf('Templates/Home', module)
 stories.addDecorator(withKnobs)
   .add('Loading', () =>
     (
-      <Home />
-    ));
+      <Home loading={true} items={[
+        { name: "Chicken" },
+        { name: "Duck" },
+        { name: "Roast" },
+        { name: "Something" },
+        { name: "Chicken" },
+        { name: "Duck" },
+        { name: "Chicken" },
+        { name: "Chicken" },
+        { name: "Chicken" },
+        { name: "Duck" },
+        { name: "Chicken" },
+        { name: "Chicken" },
+      ]} />
+    ))
+  .add('Active', () =>
+    (
+      <Home items={[
+        { name: "Chicken" },
+        { name: "Duck" },
+        { name: "Roast" },
+        { name: "Something" },
+        { name: "Chicken" },
+        { name: "Duck" },
+        { name: "Roast" },
+        { name: "Something" },
+        { name: "Chicken" },
+        { name: "Duck" },
+        { name: "Roast" },
+        { name: "Something" },
+        { name: "Chicken" },
+      ]} />
+    ))
+  ;
 
 
--- src/components/Templates/Home/index.js
+++ src/components/Templates/Home/index.js
@@ -1,46 +1,53 @@
 import React, { Component } from 'react'
 import './style.css'
 import MiniCard from '../../../components/Organisms/Card/MiniCard'
+import { Link } from 'react-router-dom'
+
+//TODO as the component is waiting for data, 
+//TODO --drop loading items around 12 to show loading 
 
 export class Home extends Component {
 
-
   render() {
 
-    const { items } = this.props;
+    const { items, loading } = this.props;
     const length = items.length;
 
-    return (
-      <div className={"home container"}>
-        {
-          items.map((item, key) => {
-            return (
-              <MiniCard loading />
-            )
-          })
-        }
+    if (length > 0) {
+      return (
+        <div className={"home container"}>
+          {
+            items.map((item, key) => {
+              return (
+                <Link to={'/item/' + item.id} key={key}>
+                  <MiniCard name={item.name} image={loading ? 'null' : item.url} loading={loading} />
+                </Link>
+              )
+            })
+          }
+        </div>
+      )
+    } else {
+      return (
+        <div className={"home container"}>
+          <MiniCard loading={true} />
+          <MiniCard loading={true} />
+          <MiniCard loading={true} />
+          <MiniCard loading={true} />
+          <MiniCard loading={true} />
+          <MiniCard loading={true} />
+          <MiniCard loading={true} />
+          <MiniCard loading={true} />
+        </div>
+      )
+    }
 
-      </div>
-    )
   }
 }
 
 Home.defaultProps = {
-  items: [
-    { name: "Chicken" },
-    { name: "Duck" },
-    { name: "Roast" },
-    { name: "Something" },
-    { name: "Chicken" },
-    { name: "Duck" },
-    { name: "Roast" },
-    { name: "Something" },
-    { name: "Chicken" },
-    { name: "Duck" },
-    { name: "Roast" },
-    { name: "Something" },
-    { name: "Chicken" },
-  ]
+  items: [],
+  loading: false
 }
 
 export default Home
