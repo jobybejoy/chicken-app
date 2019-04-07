ab01ef5b2ed4bbfe7b1cbece0c672416d9b2e813
Joby Bejoy <jobyvarghesebejoy@karunya.edu.in>
2019-04-07 16:35:56 +0400

    Redux and Firebase | App configurations

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import logo from '../assets/images/logo.svg';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import '../style/app.css';
+
+import Home from '../containers/Home'
+import ItemDetails from '../containers/ItemDetails'
+
+//* Please Remove all temp after use
+import NavBar from '../containers/temp/NavBar'
+import SignIn from '../containers/temp/SignIn'
+import AuthRoute from '../containers/temp/AuthRoute'
+import GoogleSignIn from '../containers/temp/Sign/GoogleLogin'
+
+import Cart from '../containers/Cart'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar ></NavBar>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/item/:id" component={ItemDetails} />
+            <Route path="/signin" component={SignIn} />
+
+            <Route path="/google" component={GoogleSignIn} />
+
+            <Route path="/cart" component={Cart} />
+
+            {/* Auth Route Works!! */}
+            <AuthRoute path='/gold' component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/app.story.js
+++ src/components/app.story.js
@@ -4,10 +4,10 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Welcome } from '@storybook/react/demo';
-
+import App from './app'
 // Todo : App Desc for story book home! 
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
+storiesOf('App', module)
+  .add('to Storybook', () => <App />)
+  ;
 
