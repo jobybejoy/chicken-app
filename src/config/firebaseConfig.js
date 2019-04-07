a507b87952505b7d04ae7723d3641c2438262b50
Joby Bejoy <jobyvarghesebejoy@karunya.edu.in>
2019-04-07 16:34:02 +0400

    Added Firebase , Redux and connected them up

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBiJWGAHv79c27frgUgIG3WbTa3vSSyp9A",
+  authDomain: "chicken-app-dev.firebaseapp.com",
+  databaseURL: "https://chicken-app-dev.firebaseio.com",
+  projectId: "chicken-app-dev",
+  storageBucket: "chicken-app-dev.appspot.com",
+  messagingSenderId: "1085698517998"
+};
+
+firebase.initializeApp(config);
+// firebase.firestore().settings({ timestampsInSnapshots: true })
+firebase.firestore();
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,36 @@ import './style/index.css';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import ReduxThunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+
+import FirebaseConfig from './config/firebaseConfig'
+
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(ReduxThunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(FirebaseConfig),
+    reactReduxFirebase(FirebaseConfig, { useFirestoreForProfile: true, userProfile: 'users', attachAuthIsReady: true })
+  )
+);
+
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , document.getElementById('root')
+  );
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+})
+
+
