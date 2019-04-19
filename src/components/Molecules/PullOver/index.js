1f4366e9f5a5ca38a6663473e47345ac1421b546
Joby Bejoy <jobyvarghesebejoy@karunya.edu.in>
2019-04-19 18:06:50 +0400

    Pull Over Minor Changes

--- src/components/Molecules/PullOver/index.js
+++ src/components/Molecules/PullOver/index.js
@@ -2,22 +2,31 @@ import React, { Component } from 'react'
 import Text from '../../Atoms/Text'
 import Button from '../../Atoms/Button'
 import './style.css'
+import { Redirect } from 'react-router-dom'
 
 // TODO May have to change the main content, or make varients if needed.
 
 export class PullOver extends Component {
+
+  goTo(url) {
+
+  }
+
   render() {
 
-    const { label, price, cta_label } = this.props
+    const { label, price, cta_label, cta_to } = this.props
 
     return (
-      <div className='pullOver container'>
-        <div className="content">
-          <Text className="total_label" component="div" varient={'h4'}>{label}</Text>
-          <Text className="total_value" component="div" varient={'h4'}>{price}</Text>
+      <div className="pullContainer">
+        <div className='pullOver container'>
+          <div className="content">
+            <Text className="total_label" component="div" varient={'h4'}>{label}</Text>
+            <Text className="total_value" component="div" varient={'h4'}>{price}</Text>
+          </div>
+          <Button className="cta_btn" varient={'PRIMARY'} name={'primary'} onClick={() => { this.props.history.push(cta_to) }}>{cta_label}</Button>
         </div>
-        <Button className="cta_btn" varient={'PRIMARY'} name={'primary'}>{cta_label}</Button>
       </div>
+
     )
   }
 }
@@ -26,6 +35,7 @@ PullOver.defaultProps = {
   label: 'TOTAL',
   price: '0 AED',
   cta_label: 'CHECKOUT',
+  cta_to: '/'
 };
 
 export default PullOver
